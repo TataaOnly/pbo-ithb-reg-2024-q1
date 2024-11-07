f940e9a964c4a1dbaf79f05d9e961afd7610ed29
prabhasetael <elangsatya08@gmail.com>
2024-11-07 15:49:41 +0700

    final commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+import control.*;
+import view.PrintReceipt;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("test");
+        PrintData();
     }
 }
--- src/view/PrintReceipt.java
+++ src/view/PrintReceipt.java
@@ -1,22 +1,25 @@
 package view;
 
-import model.*;
-import control.*;
 import java.util.Scanner;
-
+import control.Control;
 public class PrintReceipt {
 
     static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void PrintData(){
         System.out.println("Masukan level \n" + "1. Ekonomi \n" + "2. Bisnis \n" + "3. First Class");
         int level = scan.nextInt();
-
+    
         switch (level) {
             case 1:
-                System.out.println(itungEkonomi());
+                System.out.println("Level: Ekonomi\n" + "Total Harga:" + itungEkonomi());
                 break;
-
+            case 2:
+                System.out.println("Level: Bisnis\n" + "Total Harga" + itungBisnis());
+                break;
+            case 3:
+                System.out.println("Level: First Class\n" + "Total Harga" + itungFirst());
+                break;
             default:
                 break;
         }
